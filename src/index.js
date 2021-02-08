50ded84f97addf4ddde9728d02dd7c81a6f8ef04
Shomari Roberts <shomariroberts@gmail.com>
2021-02-08 00:15:28 -0500

    converted App to class component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  render(
-    <>
-      <h1>Hello!</h1>
-    </>
-  );
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>Latitude:</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
