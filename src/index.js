83132ddec5bf55fc0d863bd26bdba50c3266103c
Shomari Roberts <shomariroberts@gmail.com>
2021-02-08 01:26:30 -0500

    passed props down to SeasonDisplay

--- src/index.js
+++ src/index.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null, errorMessage: '' };
+    this.state = { lat: null, errorMessage: '', isLoading: false };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({
-          lat: position.coords.latitude,
-        });
-      },
-      (err) => {
-        this.setState({
-          errorMessage: err.message,
-        });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
     return (
       <>
-        {!this.state.errorMessage ? (
-          <div>Latitude: {this.state.lat}</div>
+        {this.state.isLoading ? (
+          <div>Loading...</div>
         ) : (
-          <div>Error: {this.state.errorMessage}</div>
+          <div>
+            {!this.state.errorMessage ? (
+              <SeasonDisplay lat={this.state.lat} />
+            ) : (
+              <div>Error: {this.state.errorMessage}</div>
+            )}
+          </div>
         )}
       </>
     );
