1655aa24bab05fdad1a91fa908e7500e92638f96
Shomari Roberts <shomariroberts@gmail.com>
2021-02-08 00:51:36 -0500

    added error handling and conditional lat vs error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -13,12 +13,24 @@ class App extends React.Component {
           lat: position.coords.latitude,
         });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({
+          errorMessage: err.message,
+        });
+      }
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+      <>
+        {!this.state.errorMessage ? (
+          <div>Latitude: {this.state.lat}</div>
+        ) : (
+          <div>Error: {this.state.errorMessage}</div>
+        )}
+      </>
+    );
   }
 }
 
